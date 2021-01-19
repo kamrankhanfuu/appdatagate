dca2be10c1b2fc90563fdab15248f24081f7666c
kamrankhanfuu <77056144+kamrankhanfuu@users.noreply.github.com>
2021-01-19 19:12:10 +0100

    updates

--- Data/DataGate.Data.Common/Models/BaseDeletableModel.cs
+++ Data/DataGate.Data.Common/Models/BaseDeletableModel.cs
@@ -5,7 +5,6 @@
     public abstract class BaseDeletableModel<TKey> : BaseModel<TKey>, IDeletableEntity
     {
         public bool IsDeleted { get; set; }
-
         public DateTime? DeletedOn { get; set; }
     }
 }
